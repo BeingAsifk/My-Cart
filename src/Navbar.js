dacd87cbe3f76d82bbac1cc5ca2a92c971979d7e
Asif Khan <beingasifk07@gmail.com>
2024-02-18 22:40:42 +0530

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useGlobalContext } from "./context";
+import bag from "./media/bag.svg";
+
+const Navbar = () => {
+  const { amount } = useGlobalContext();
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3>hotDeals</h3>
+        <div className="nav-container">
+          <img src={bag} alt="bag" height="50px" width="50px" />
+          <div className="amount-container">
+            <p className="total-amount">{amount}</p>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    title: "MacBook Pro",
+    price: "96,000",
+    img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQKHqMBQLmtF3gDtDmChwHhcE35SGGIL-4UI6OnT1btaEuo-pNiLjtMdN5E9ipksdBHehVAnpEL4raPw6tHUqMYYVYbDy70Q-9haZo0Wg2vMFZlYe4cWyvyJvGGjpZnNsAE_nCo3J0&usqp=CAc",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "google pixel ",
+    price: 499.99,
+    img: "https://lh3.googleusercontent.com/cSw-q_OfYWDmzyGcXnMQz8HY2fuOoJYG-wlztDJHCdLR9VyIs4emlOziURSotCWE2aA5pUe6BdrjFPMjLFllDRCxCvvqunygl4s=rw-e365-w2046",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "Xiaomi Redmi Note 2",
+    price: 699.99,
+    img: "https://fdn2.gsmarena.com/vv/bigpic/xiaomi-redmi-note-2--.jpg",
+    amount: 1,
+  },
+];
